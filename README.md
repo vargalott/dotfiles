86052d5d8993e93d4db5034cafbeed360d019b2e
vargalott <156085832+vargalott@users.noreply.github.com>
2021-06-07 12:03:47 +0300

    :octocat:

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 # dotfiles
-My dotfiles
+
+Personal dotfiles
+
 ## Usage
+
 Run the command:
 ```sh
-    $ ./run.sh
+$ ./run.sh
 ```
+
 Options:
 * _load_: load current config
 * _install_: apply configs
 
 ## Credits
- * [ngynLk polybar theme](https://github.com/ngynLk/polybar-themes/tree/master/san)
- * [Avasz rofi theme](https://github.com/Avasz/rofi-themes-avasz/blob/master/darker-than-black/configs/darker-than-black_v3.rasi)
+
+* [ngynLk polybar theme](https://github.com/ngynLk/polybar-themes/tree/master/san)
+* [Avasz rofi theme](https://github.com/Avasz/rofi-themes-avasz/blob/master/darker-than-black/configs/darker-than-black_v3.rasi)
 
 Special thanks to my friend [IceArrow256](https://github.com/IceArrow256) for the invaluable help in everything.
+
+## License
+
+This project is licensed under the [UNLICENSE](LICENSE).
--- run.sh
+++ run.sh
@@ -1,50 +1,51 @@
 #!/bin/sh
 
 data_process() {
-    mode=$1
-    FROM=$2
-    TO=$3
-    if [[ "$mode" == "load" ]];
-    then
-        rsync --recursive $HOME/$FROM $PWD/$TO
-        echo -e "load $HOME/$FROM -> $PWD/$FROM"
-    elif [[ "$mode" == "install" ]];
-    then
-        rsync --recursive $PWD/$FROM $HOME/$TO
-        echo -e "install $PWD/$FROM -> $HOME/$FROM"
-    else
-        echo "$1: wrong argument: must be [load|install]" >&2
-        exit 2
-    fi
+  mode=$1
+  FROM=$2
+  TO=$3
+  
+  if [[ "$mode" == "load" ]];
+  then
+    rsync --recursive $HOME/$FROM $PWD/$TO
+    echo -e "load $HOME/$FROM -> $PWD/$FROM"
+  elif [[ "$mode" == "install" ]];
+  then
+    rsync --recursive $PWD/$FROM $HOME/$TO
+    echo -e "install $PWD/$FROM -> $HOME/$FROM"
+  else
+    echo "$1: wrong argument: must be [load|install]" >&2
+    exit 2
+  fi
 }
 
 process() {
-    mode=$1
+  mode=$1
 
-    data_process $mode .config/alacritty .config
-    data_process $mode .config/dunst .config
-    data_process $mode .config/gtk-3.0 .config
-    data_process $mode .config/i3 .config
-    data_process $mode .config/Kvantum .config
-    data_process $mode .config/picom .config
-    data_process $mode .config/polybar .config
-    data_process $mode .config/rofi .config
+  data_process $mode .config/alacritty .config
+  data_process $mode .config/dunst .config
+  data_process $mode .config/gtk-3.0 .config
+  data_process $mode .config/i3 .config
+  data_process $mode .config/Kvantum .config
+  data_process $mode .config/picom .config
+  data_process $mode .config/polybar .config
+  data_process $mode .config/rofi .config
 
-    data_process $mode .icons/default .icons
+  data_process $mode .icons/default .icons
 
-    data_process $mode .gitconfig .
-    data_process $mode .gtkrc-2.0 .
-    data_process $mode .xinitrc .
-    data_process $mode .zshrc .
+  data_process $mode .gitconfig .
+  data_process $mode .gtkrc-2.0 .
+  data_process $mode .xinitrc .
+  data_process $mode .zshrc .
 }
 
 if [[ "$#" -eq 1 ]];
 then
-    process $1
+  process $1
 
-    systemd-analyze plot > graph.svf
-    pacman -Qe > package-list.txt
+  systemd-analyze plot > graph.svf
+  pacman -Qe > package-list.txt
 else
-    echo "Usage: $0 [load|install]" >&2
-    exit 1
+  echo "Usage: $0 [load|install]" >&2
+  exit 1
 fi
