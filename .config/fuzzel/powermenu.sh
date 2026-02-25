58553ad17720be921748a523ff01aa900912bd46
Mykola Symon <vargalott@gmail.com>
2026-02-25 16:53:00 +0300

    config(fuzzel)

--- .config/fuzzel/powermenu.sh
+++ .config/fuzzel/powermenu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-command=$( echo -e "Shutdown\nReboot\nLock\nLogout" | fuzzel -d --config ~/.config/fuzzel/powermenu.ini )
+command=$( pgrep -x fuzzel >/dev/null 2>&1 || echo -e "Shutdown\nReboot\nLock\nLogout" | fuzzel -d --config ~/.config/fuzzel/powermenu.ini )
 case $command in
     "Shutdown")
         systemctl poweroff
