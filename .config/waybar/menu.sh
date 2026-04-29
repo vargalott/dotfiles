857cd12c80ca384229e362c471ed224d7eb61959
Mykola Symon <vargalott@gmail.com>
2026-04-29 13:15:59 +0300

    config(waybar)

--- .config/waybar/menu.sh
+++ .config/waybar/menu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
+if [[ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]]; then
     exec "$HOME/.config/wofi/menu.sh"
-elif [ -n "$NIRI_SOCKET" ]; then
+elif [[ -n "$NIRI_SOCKET" ]]; then
     exec "$HOME/.config/fuzzel/menu.sh"
 fi
