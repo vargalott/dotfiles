ce3755b79975c6f7d231bde7fd6869f5973c906b
Mykola Symon <vargalott@gmail.com>
2026-05-01 14:01:10 +0300

    config(wofi)

--- .config/wofi/powermenu.sh
+++ .config/wofi/powermenu.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-command=$( pgrep -x wofi >/dev/null 2>&1 || echo -e "Shutdown\nReboot\nLock\nLogout" | wofi -i --conf ~/.config/wofi/powermenu.conf --style ~/.config/wofi/style.css )
+command=$( pgrep -x wofi >/dev/null 2>&1 || echo -e "Shutdown\nReboot\nLock\nLogout" | \
+  wofi -i --conf ~/.config/wofi/powermenu.conf --style ~/.config/wofi/style.css )
+
 case $command in
-    "Shutdown")
-        systemctl poweroff
-        ;;
-    "Reboot")
-        systemctl reboot
-        ;;
-    "Lock")
-        hyprlock
-        ;;
-    "Logout")
-        hyprctl dispatch exit
-        ;;
-    *)
-        ;;
+  "Shutdown")
+    systemctl poweroff
+    ;;
+  "Reboot")
+    systemctl reboot
+    ;;
+  "Lock")
+    hyprlock
+    ;;
+  "Logout")
+    hyprctl dispatch exit
+    ;;
+  *)
+    ;;
 esac
