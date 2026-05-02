3b2f8762a21e041c0f8003a5aa6be2a5444977cc
Mykola Symon <vargalott@gmail.com>
2026-05-02 13:01:16 +0300

    config(fzf)

--- .config/fzf/powermenu.sh
+++ .config/fzf/powermenu.sh
@@ -4,19 +4,9 @@ selected="$(echo -e "Shutdown\nReboot\nLock\nLogout" | fzf --no-input --cycle )"
 [[ -z "$selected" ]] && exit
 
 case $selected in
-  "Shutdown")
-    systemctl poweroff
-    ;;
-  "Reboot")
-    systemctl reboot
-    ;;
-  "Lock")
-    setsid -f hyprlock -q
-    ;;
-  "Logout")
-    niri msg action quit -s
-    ;;
-  *)
-    ;;
+  "Shutdown")   systemctl poweroff ;;
+  "Reboot")     systemctl reboot ;;
+  "Lock")       setsid -f hyprlock -q ;;
+  "Logout")     ( [[ -n "$NIRI_SOCKET" ]] && niri msg action quit -s ) || ( [[ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]] && hyprctl dispatch exit ) ;;
 esac
 kill $PPID
