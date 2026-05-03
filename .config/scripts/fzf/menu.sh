73233f4ba00f8248eb67fe3520d12f546e545e06
Mykola Symon <vargalott@gmail.com>
2026-05-03 16:00:34 +0300

    config(scripts/fzf/menu)

--- .config/scripts/fzf/menu.sh
+++ .config/scripts/fzf/menu.sh
@@ -7,7 +7,7 @@ desktop_file() {
   find /var/lib/flatpak/exports/share/applications -name "*.desktop" 2>/dev/null
   find "$HOME/.local/share/flatpak/exports/share/applications" -name "*.desktop" 2>/dev/null
 }
-selected="$(desktop_file | awk -F/ '{name=$NF; sub(/\.desktop$/, "", name); print name}' | \
+selected="$( desktop_file | awk -F/ '{name=$NF; sub(/\.desktop$/, "", name); print name}' | \
   sort -k1,1 | cut -f2- | fzf --info-command 'echo -e "Applications ($FZF_POS/$FZF_TOTAL_COUNT)"' --cycle )"
 [[ -z "$selected" ]] && exit
 echo "$selected" | while read -r line ; do setsid -f gtk-launch "$(basename $line)"; done
