b1e8b2510b14bf9dc6cdb0b2920dc4f9ea854c7c
Mykola Symon <vargalott@gmail.com>
2026-05-03 16:01:00 +0300

    config(scripts/hypr/hyprpaper)

--- .config/scripts/hypr/hyprpaper.sh
+++ .config/scripts/hypr/hyprpaper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 killall -q hyprpaper
-while pgrep -u $UID -x hyprpaper >/dev/null; do sleep 1; done
-hyprpaper
+while pgrep -u $UID -x hyprpaper >/dev/null; do sleep 0.1; done
+setsid -f hyprpaper
