d87eacb48ce431853f857baeb0f9326d25a348f0
Mykola Symon <vargalott@gmail.com>
2026-05-03 16:01:09 +0300

    config(scripts/waybar)

--- .config/scripts/waybar/waybar.sh
+++ .config/scripts/waybar/waybar.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 killall -q waybar
-while pgrep -u $UID -x waybar >/dev/null; do sleep 1; done
-waybar
+while pgrep -u $UID -x waybar >/dev/null; do sleep 0.1; done
+setsid -f waybar
