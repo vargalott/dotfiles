73e9abde6aeea676753ab831c70b0e3c43900daf
Mykola Symon <vargalott@gmail.com>
2026-05-26 09:04:04 +0300

    config(scripts/common/theme.sh)

--- .config/scripts/common/theme.sh
+++ .config/scripts/common/theme.sh
@@ -4,9 +4,9 @@ CURRENT_THEME=$(gsettings get org.gnome.desktop.interface color-scheme | tr -d "
 case $CURRENT_THEME in
   "prefer-light"|"default")
     # gtk
-    gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-    gsettings set org.gnome.desktop.interface gtk-theme "gruvbox-dark"
-    gsettings set org.gnome.desktop.interface icon-theme "gruvbox-icons-dark"
+    gsettings set org.gnome.desktop.interface color-scheme  "prefer-dark"
+    gsettings set org.gnome.desktop.interface gtk-theme     "gruvbox-dark"
+    gsettings set org.gnome.desktop.interface icon-theme    "gruvbox-icons-dark"
     sed -i --follow-symlinks \
       -e "s/^gtk-application-prefer-dark-theme=.*/gtk-application-prefer-dark-theme=1/" \
       -e "s/^gtk-theme-name=.*/gtk-theme-name=gruvbox-dark/" \
@@ -22,20 +22,24 @@ case $CURRENT_THEME in
     kvantummanager --set gruvbox-dark
 
     # app specific
-    ln -sf gruvbox-dark.toml $HOME/.config/alacritty/theme.toml
-    ln -sf gruvbox-dark.dunstrc $HOME/.config/dunst/dunstrc
-    ln -sf gruvbox-dark.menu.ini $HOME/.config/fuzzel/menu.ini
-    ln -sf gruvbox-dark.powermenu.ini $HOME/.config/fuzzel/powermenu.ini
-    ln -sf gruvbox-dark.conf $HOME/.config/kitty/theme.conf
-    ln -sf gruvbox-dark.kdl $HOME/.config/niri/theme.kdl
-    ln -sf gruvbox-dark.css $HOME/.config/waybar/colors.css
-    ln -sf gruvbox-dark.css $HOME/.config/wofi/colors.css
+    ln -sf gruvbox-dark.toml              $HOME/.config/alacritty/theme.toml
+    ln -sf gruvbox-dark.dunstrc           $HOME/.config/dunst/dunstrc
+    ln -sf gruvbox-dark.menu.ini          $HOME/.config/fuzzel/menu.ini
+    ln -sf gruvbox-dark.powermenu.ini     $HOME/.config/fuzzel/powermenu.ini
+    ln -sf gruvbox-dark.conf              $HOME/.config/kitty/theme.conf
+    ln -sf gruvbox-dark.kdl               $HOME/.config/niri/theme.kdl
+    ln -sf gruvbox-dark.css               $HOME/.config/waybar/colors.css
+    ln -sf gruvbox-dark.css               $HOME/.config/wofi/colors.css
+
+    sed -i --follow-symlinks \
+      -e "s|^color_theme = .*|color_theme = \"$HOME/.config/btop/themes/gruvbox-dark.theme\"|" \
+      "$HOME/.config/btop/btop.conf"
   ;;
   "prefer-dark")
     # gtk
-    gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
-    gsettings set org.gnome.desktop.interface gtk-theme "gruvbox-light"
-    gsettings set org.gnome.desktop.interface icon-theme "gruvbox-icons-light"
+    gsettings set org.gnome.desktop.interface color-scheme  "prefer-light"
+    gsettings set org.gnome.desktop.interface gtk-theme     "gruvbox-light"
+    gsettings set org.gnome.desktop.interface icon-theme    "gruvbox-icons-light"
     sed -i --follow-symlinks \
       -e "s/^gtk-application-prefer-dark-theme=.*/gtk-application-prefer-dark-theme=0/" \
       -e "s/^gtk-theme-name=.*/gtk-theme-name=gruvbox-light/" \
@@ -51,14 +55,18 @@ case $CURRENT_THEME in
     kvantummanager --set gruvbox-light
 
     # app specific
-    ln -sf gruvbox-light.toml $HOME/.config/alacritty/theme.toml
-    ln -sf gruvbox-light.dunstrc $HOME/.config/dunst/dunstrc
-    ln -sf gruvbox-light.menu.ini $HOME/.config/fuzzel/menu.ini
-    ln -sf gruvbox-light.powermenu.ini $HOME/.config/fuzzel/powermenu.ini
-    ln -sf gruvbox-light.conf $HOME/.config/kitty/theme.conf
-    ln -sf gruvbox-light.kdl $HOME/.config/niri/theme.kdl
-    ln -sf gruvbox-light.css $HOME/.config/waybar/colors.css
-    ln -sf gruvbox-light.css $HOME/.config/wofi/colors.css
+    ln -sf gruvbox-light.toml             $HOME/.config/alacritty/theme.toml
+    ln -sf gruvbox-light.dunstrc          $HOME/.config/dunst/dunstrc
+    ln -sf gruvbox-light.menu.ini         $HOME/.config/fuzzel/menu.ini
+    ln -sf gruvbox-light.powermenu.ini    $HOME/.config/fuzzel/powermenu.ini
+    ln -sf gruvbox-light.conf             $HOME/.config/kitty/theme.conf
+    ln -sf gruvbox-light.kdl              $HOME/.config/niri/theme.kdl
+    ln -sf gruvbox-light.css              $HOME/.config/waybar/colors.css
+    ln -sf gruvbox-light.css              $HOME/.config/wofi/colors.css
+
+    sed -i --follow-symlinks \
+      -e "s|^color_theme = .*|color_theme = \"$HOME/.config/btop/themes/gruvbox-light.theme\"|" \
+      "$HOME/.config/btop/btop.conf"
   ;;
   *) ;;
 esac
